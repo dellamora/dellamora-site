53032d083b8e6c5f24116bc59aea7e6495d8f266
dellamora <francielle@dellamora.dev>
2022-11-23 20:32:40 -0300

    added content in hero component

---
+++ src/modules/hero/cards/Cards.tsx
@@ -0,0 +1,30 @@
+import { motion } from "framer-motion";
+import React from "react";
+
+const Cards: React.FC = (): JSX.Element => {
+  return (
+    <div className="flex justify-center">
+      <motion.div
+        animate={{ rotate: -6 }}
+        transition={{ duration: 2, delay: 0.75 }}
+        className="
+        bg-[#1E1E1E]
+        lg:dark:bg-[#131313]
+          lg:w-[21rem] 
+          lg:rounded-[16px] 
+          lg:h-[25rem] 
+          "
+      />
+      <img
+        src="/mePc.jpg"
+        className="
+        
+          hidden lg:block lg:absolute
+          rounded-[16px] w-[21rem] h-[25rem]
+          "
+      />
+    </div>
+  );
+};
+
+export default Cards;
--- src/modules/hero/index.tsx
+++ src/modules/hero/index.tsx
@@ -2,13 +2,30 @@
 
 import React from "react";
 import Section from "../../common/components/section";
+import Cards from "./cards/Cards";
+import Instagram from "../../common/svgs/instagramIcon";
+import GitHub from "../../common/svgs/gitHub";
+import Twitter from "../../common/svgs/twitterIcon";
+import SendMail from "../../common/svgs/sendMail";
+import LinkedIn from "../../common/svgs/linkedinIcon";
 
 const Hero: React.FC = (): JSX.Element => {
   return (
-    <Section id="/">
+    <Section id="/" className=" flex justify-center items-center lg:gap-80">
       <div>
-        <h1> Hello, AboutMe!</h1>
+        <p className="font-Inter dark:text-[#c2bfbf] max-w-[33rem] text-justify text-[#232E35]">
+          Creative front-end developer with more than +2 years of Experience in
+          enterprise companies and startups. Proficient in Html, Tailwind Css,
+          Javascript and React. Passionate about UI/UX
+        </p>
+        <div className="flex justify-center lg:justify-start space-x-8 items-center py-11">
+          <Instagram className="stroke-[#656D72]" width="20px" />
+          <GitHub className="stroke-[#656D72]" width="20px" />
+          <Twitter className="stroke-[#656D72]" width="22px" />
+          <SendMail className="stroke-[#656D72]" width="23px" />
+        </div>
       </div>
+      <Cards />
     </Section>
   );
 };
