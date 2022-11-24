d4c63bc201a549efabb5f09e23bfbdd96f966aee
dellamora <francielle@dellamora.dev>
2022-11-24 11:47:55 -0300

    feat: create a standalone section title component to use in the section

--- src/common/components/section.tsx
+++ src/common/components/section.tsx
@@ -1,6 +1,6 @@
 import { useInView } from "react-intersection-observer";
 import { useEffect } from "react";
-import { useAnimation, motion } from "framer-motion";
+import SectionTitle from "./sectionTitle";
 type Props = {
   id: string;
   children: JSX.Element | JSX.Element[];
@@ -16,45 +16,14 @@ export const Section = ({
   children,
   bgColor,
   setIsInView,
+  className,
   title,
   subTitle,
-  className,
 }: Props): JSX.Element => {
-  const ctrls = useAnimation();
-
   const { ref, inView } = useInView({
-    threshold: 0.5,
+    threshold: 0.2,
     triggerOnce: true,
   });
-
-  useEffect(() => {
-    if (inView) {
-      ctrls.start("visible");
-    }
-    if (!inView) {
-      ctrls.start("hidden");
-    }
-  }, [ctrls, inView]);
-
-  const wordAnimation = {
-    hidden: {},
-    visible: {},
-  };
-
-  const characterAnimation = {
-    hidden: {
-      opacity: 0,
-      y: `0.25em`,
-    },
-    visible: {
-      opacity: 1,
-      y: `0em`,
-      transition: {
-        duration: 1,
-        ease: [0.2, 0.65, 0.3, 0.9],
-      },
-    },
-  };
 
   useEffect(() => {
     if (setIsInView) {
@@ -63,89 +32,15 @@ export const Section = ({
   }, [inView]);
   return (
     <section
-      className={`relative overflow-x-hidden p-[65px] h-[90vh] bg-[#FBFAFF] dark:bg-[#1E1E1E] dark:text-[#FBFAFF] ${className}`}
+      className={`relative overflow-x-hidden lg:p-[65px] p-5 h-[90vh] bg-[#FBFAFF] dark:bg-[#1E1E1E] dark:text-[#FBFAFF] ${className}`}
       ref={ref}
     >
       <div id={id} className="absolute pointer-events-none -top-[100px]" />
-      {(title || subTitle) && (
-        <div>
-          {subTitle && (
-            <div className="flex md:items-center ">
-              <motion.div
-                initial="hidden"
-                animate={ctrls}
-                variants={characterAnimation}
-                transition={{
-                  delay: 0.5,
-                }}
-                className=" hidden md:flex mr-3 h-0.5 w-4 bg-[#D9D9D9] justify-center"
-              />
-              {subTitle.split(" ").map((word, index) => {
-                return (
-                  <motion.span
-                    className=" font-Inter text-subTitle mr-2 text-[#656D72] uppercase text-bold "
-                    ref={ref}
-                    aria-hidden="true"
-                    key={index}
-                    initial="hidden"
-                    animate={ctrls}
-                    variants={wordAnimation}
-                    transition={{
-                      delayChildren: index * 0.25,
-                      staggerChildren: 0.05,
-                    }}
-                  >
-                    {word.split("").map((character, index) => {
-                      return (
-                        <motion.span
-                          aria-hidden="true"
-                          key={index}
-                          variants={characterAnimation}
-                        >
-                          {character}
-                        </motion.span>
-                      );
-                    })}
-                  </motion.span>
-                );
-              })}
-            </div>
-          )}
-          {title && (
-            <div className="flex md:items-center">
-              {title.split(" ").map((word, index) => {
-                return (
-                  <motion.span
-                    className=" font-Frank text-title mr-2 dark:text-[#dbdbdb]  text-black uppercase text-bold "
-                    ref={ref}
-                    aria-hidden="true"
-                    key={index}
-                    initial="hidden"
-                    animate={ctrls}
-                    variants={wordAnimation}
-                    transition={{
-                      delayChildren: index * 0.25,
-                      staggerChildren: 0.05,
-                    }}
-                  >
-                    {word.split("").map((character, index) => {
-                      return (
-                        <motion.span
-                          aria-hidden="true"
-                          key={index}
-                          variants={characterAnimation}
-                        >
-                          {character}
-                        </motion.span>
-                      );
-                    })}
-                  </motion.span>
-                );
-              })}
-            </div>
-          )}
-        </div>
-      )}
+      <SectionTitle
+        triggerAnimation={inView}
+        title={title}
+        subTitle={subTitle}
+      />
       {children}
     </section>
   );
---
+++ src/common/components/sectionTitle.tsx
@@ -0,0 +1,123 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+
+import { motion } from "framer-motion";
+
+type Props = {
+  title?: string;
+  subTitle?: string;
+  className?: string;
+  triggerAnimation: boolean;
+};
+
+const SectionTitle = ({
+  title,
+  subTitle,
+  triggerAnimation,
+}: Props): JSX.Element => {
+  const wordAnimation = {
+    hidden: {},
+    visible: {},
+  };
+
+  const characterAnimation = {
+    hidden: {
+      opacity: 0,
+      y: `0.25em`,
+    },
+    visible: {
+      opacity: 1,
+      y: `0em`,
+      transition: {
+        duration: 1,
+        ease: [0.2, 0.65, 0.3, 0.9],
+      },
+    },
+  };
+  if (!title && !subTitle) {
+    return null;
+  }
+  return (
+    <div>
+      {subTitle && (
+        <div className="flex md:items-center ">
+          <motion.div
+            initial="hidden"
+            animate={triggerAnimation ? "visible" : "hidden"}
+            variants={characterAnimation}
+            transition={{
+              delay: 0.5,
+            }}
+            className="hidden md:flex mr-3 h-0.5 w-4 bg-[#D9D9D9] "
+          />
+          {subTitle.split(" ").map((word, index) => {
+            return (
+              <motion.span
+                className=" font-Inter text-subTitle mr-2 text-[#656D72] uppercase text-bold "
+                aria-hidden="true"
+                key={index}
+                initial="hidden"
+                animate={triggerAnimation ? "visible" : "hidden"}
+                variants={wordAnimation}
+                transition={{
+                  delayChildren:
+                    index === 0
+                      ? 0
+                      : subTitle.split(" ").slice(0, index).join(" ").length *
+                        0.1,
+                  staggerChildren: 0.1,
+                }}
+              >
+                {word.split("").map((character, index) => {
+                  return (
+                    <motion.span
+                      aria-hidden="true"
+                      key={index}
+                      variants={characterAnimation}
+                    >
+                      {character}
+                    </motion.span>
+                  );
+                })}
+              </motion.span>
+            );
+          })}
+        </div>
+      )}
+      <div className="flex md:items-center">
+        {title.split(" ").map((word, index) => {
+          return (
+            <motion.span
+              className=" font-Frank font-bold text-2xl lg:text-title mr-2 dark:text-[#dbdbdb]  text-black uppercase text-bold "
+              aria-hidden="true"
+              key={index}
+              initial="hidden"
+              animate={triggerAnimation ? "visible" : "hidden"}
+              variants={wordAnimation}
+              transition={{
+                delayChildren:
+                  index === 0
+                    ? 0
+                    : title.split(" ").slice(0, index).join(" ").length * 0.1,
+                staggerChildren: 0.1,
+              }}
+            >
+              {word.split("").map((character, index) => {
+                return (
+                  <motion.span
+                    aria-hidden="true"
+                    key={index}
+                    variants={characterAnimation}
+                  >
+                    {character}
+                  </motion.span>
+                );
+              })}
+            </motion.span>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default SectionTitle;
