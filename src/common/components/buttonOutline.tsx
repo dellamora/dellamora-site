147701bf3ddf6f3f1776c4e0284c6df2a8d0d3ec
dellamora <francielle@dellamora.dev>
2022-11-25 11:22:43 -0300

    add button component

---
+++ src/common/components/buttonOutline.tsx
@@ -0,0 +1,45 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+
+import Link from "next/link";
+import React, { useState } from "react";
+
+type Props = {
+  href?: string;
+  children?: string;
+  isLink: boolean;
+  className?: string;
+};
+
+const ButtonOutline = ({
+  href,
+  children,
+  isLink,
+  className,
+}: Props): JSX.Element => {
+  const style = ` 
+    dark:text-[#FBFAFF] 
+    dark:border-[#FBFAFF]  
+    hover:dark:bg-[#3b3b3b]/50
+    hover:bg-gray-100 
+    font-semibold 
+    py-[13px] px-[20px]
+    border 
+    text-[#232E35]
+    font-Inter 
+    rounded-lg ${className}`;
+  const buttonProps = {
+    href: href,
+    className: style,
+  };
+  return (
+    <>
+      {isLink ? (
+        <Link {...buttonProps}>{children}</Link>
+      ) : (
+        <a {...buttonProps}>{children}</a>
+      )}
+    </>
+  );
+};
+
+export default ButtonOutline;
