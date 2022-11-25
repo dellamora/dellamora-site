ba097e8c3b9c70190424f3be34c83e54a3604194
dellamora <francielle@dellamora.dev>
2022-11-25 11:25:23 -0300

    add ButtonOutline in header

--- src/modules/contact/index.tsx
+++ src/modules/contact/index.tsx
@@ -1,14 +1,21 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 
 import React from "react";
+import ButtonOutline from "../../common/components/buttonOutline";
 import Section from "../../common/components/section";
 
-/* import { Container } from "./styles"; */
-
-const Contact: React.FC = (): JSX.Element => {
+const Contact = (): JSX.Element => {
   return (
-    <Section id="contact">
-      <h1> Hello, Contact!</h1>
+    <Section id="contact" className="p-4">
+      <h1> Lets create something together</h1>
+      <p className="w-80">
+        I’m interested in freelance opportunities – especially ambitious or
+        large projects. However, if you have other request or question, don’t
+        hesitate to use the form.
+      </p>
+      <ButtonOutline href="mailto:francielle@dellamora.dev" isLink={false}>
+        Send Mail
+      </ButtonOutline>
     </Section>
   );
 };
