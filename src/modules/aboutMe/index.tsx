2ae7834dc0b2de6d6ad5bb9ec31933da49fa80cc
Dellamora <francielle@dellamora.dev>
2023-01-02 19:54:11 -0300

    add my real informations

--- src/modules/aboutMe/index.tsx
+++ src/modules/aboutMe/index.tsx
@@ -9,12 +9,12 @@ import SectionTitle from "../../common/components/sectionTitle";
 const AboutMe: React.FC = (): JSX.Element => {
   const [inView, setInview] = useState(false);
   const skills = [
-    { name: "TypeScript", value: 100 },
-    { name: "NextJS", value: 100 },
-    { name: "TailWind CSS", value: 100 },
-    { name: "Express.js", value: 100 },
-    { name: "Prisma/SQL/NoSQl", value: 100 },
-    { name: "Firebase", value: 100 },
+    { name: "TailWind CSS", value: 80 },
+    { name: "NextJS", value: 70 },
+    { name: "TypeScript", value: 66 },
+    { name: "Firebase", value: 64 },
+    { name: "Express.js", value: 62 },
+    { name: "Prisma/SQL/NoSQl", value: 60 },
   ];
   return (
     <Section
@@ -29,28 +29,28 @@ const AboutMe: React.FC = (): JSX.Element => {
         subTitle="Learning path"
         triggerAnimation={inView}
       />
-      <motion.div 
+      <motion.div
         className="flex flex-col md:flex-row gap-6  justify-center items-start"
         initial="hidden"
-        animate={inView ? "view" : "hidden"}  
+        animate={inView ? "view" : "hidden"}
       >
-        <motion.div 
+        <motion.div
           className="md:w-1/2"
-          initial={{opacity: 0}}
-          animate={inView ?  {opacity: 1, transition:{delay: 1.6}} : {opacity: 0}} 
+          initial={{ opacity: 0 }}
+          animate={
+            inView ? { opacity: 1, transition: { delay: 1.6 } } : { opacity: 0 }
+          }
         >
-          <p id={"first-p"}>
-          orem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
-            {/* Im a Full-Stack developer with a passion for creating amazing code
-            and solving problems. Im always looking for new challenges and
-            opportunities to learn and grow. As a Full-Stack Developer I have 2
-            main Languages, TypeScript and Python. Python being most for Data
-            Science and automations, and Typescript being the main language for
-            Web Development. Currently Web Development is my main focus, using
-            tools like NextJS, ExpressJS, MongoDB / PostgresSQL and Apollo I can
-            do almost everything! I also have experience with Docker and Jest to
-            make sure my code works as intended. You can check the About Me Page
-            if you want to know more! */}
+          <p id={"first-p"} className="text-justify text-lg ">
+            My passion for computers began when I was a child and used my
+            father&apos;s computer to play games. Over the years I learned how
+            to create themes on the Tumblr platform and I consider it a
+            milestone in my choice of profession, since I was messing around
+            with HTML and CSS and got enchanted with the web universe.
+            Currently, I have over 2 years of experience working in the software
+            development industry, seeking efficiency in developing ideas and
+            turning them into reality, bringing smart solutions to challenges
+            and adversities.
           </p>
         </motion.div>
         <motion.div
@@ -60,7 +60,7 @@ const AboutMe: React.FC = (): JSX.Element => {
           {skills.map((skills, i) => (
             <div key={`skill-${i}`}>
               <h4>{skills.name}</h4>
-              <ProgressBar width="w-[60%]" />
+              <ProgressBar width={skills.value} />
             </div>
           ))}
         </motion.div>
