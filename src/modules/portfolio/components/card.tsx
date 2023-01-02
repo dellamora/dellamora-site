42a9ac96021ca3e9ef245e6336797b1fe87aa5ce
Dellamora <francielle@dellamora.dev>
2023-01-02 17:51:47 -0300

    eslint and refactores sizes and style button

--- src/modules/portfolio/components/card.tsx
+++ src/modules/portfolio/components/card.tsx
@@ -3,51 +3,49 @@
 import { motion } from "framer-motion";
 import Image from "next/image";
 import { useEffect, useState } from "react";
-import  useWindowDimensions  from "../../../common/hooks/useWindowDimensions";
-import {Projects} from "../../../domain/interfaces"
+import useWindowDimensions from "../../../common/hooks/useWindowDimensions";
+import { Projects } from "../../../domain/interfaces";
 type Props = {
-  project: Pick<
-  Projects,
-    "name" 
-  >;
-  inView: boolean
-  onClick: () => void
+  project: Pick<Projects, "name">;
+  inView: boolean;
+  onClick: () => void;
 };
-const CardProject = ({ project , onClick, inView}: Props): JSX.Element => {
-  const [mounted, setMounted] = useState(false)
+const CardProject = ({ project, onClick, inView }: Props): JSX.Element => {
+  const [mounted, setMounted] = useState(false);
   useEffect(() => {
-    setMounted(true)
-  } ,[])
-  const {isMobile} = useWindowDimensions()
+    setMounted(true);
+  }, []);
+  const { isMobile } = useWindowDimensions();
   return (
-    <motion.div 
-      className="relative h-56 rounded overflow-hidden flex justify-start max-w-sm" 
+    <motion.div
+      className="relative h-56 rounded overflow-hidden flex justify-start max-w-sm"
       onClick={onClick}
       whileHover={{ opacity: 1 }}
       initial="hidden"
       animate={inView ? "view" : "hidden"}
       variants={{
-        hidden: {opacity: 0}, 
-        view: { 
-          opacity:  1 , 
-          transition:{delay: 2}
-        }
-      }}>
-      {mounted && 
-            <motion.div
-            className="flex flex-col self-end justify-end items-start z-10 h-full w-full "
-            initial={{
-              opacity: 0,
-            }}
-            animate={{opacity: isMobile ? 1 : 0}}
-           whileHover={{ opacity: 1 }}
-          >
-            <div className="bg-secondaryDark/50 w-full h-1/3 p-3 z-20">
+        hidden: { opacity: 0 },
+        view: {
+          opacity: 1,
+          transition: { delay: 2 },
+        },
+      }}
+    >
+      {mounted && (
+        <motion.div
+          className="flex flex-col self-end justify-end items-start z-10 h-full w-full "
+          initial={{
+            opacity: 0,
+          }}
+          animate={{ opacity: isMobile ? 1 : 0 }}
+          whileHover={{ opacity: 1 }}
+        >
+          <div className="bg-secondaryDark/50 w-full h-1/3 p-3 z-20">
             <h1 className="font-bold text-primary">{project.name}</h1>
-            <h4 className="text-sm text-primary" > learn more</h4>
-            </div>
-          </motion.div>
-      }
+            <h4 className="text-sm text-primary"> learn more</h4>
+          </div>
+        </motion.div>
+      )}
       <Image
         className="absolute object-cover aspect-video"
         alt="movie poster"
--- src/modules/portfolio/components/modal.tsx
+++ src/modules/portfolio/components/modal.tsx
@@ -10,70 +10,85 @@ type Props = {
   isOpen: boolean;
   onClose: () => void;
   project?: Pick<
-  Projects,
-    "name" | "description" |  "link" | "technologies" | "data"
+    Projects,
+    "name" | "description" | "link" | "technologies" | "data"
   >;
+};
 
-}
-
-export default function ModalProject({  isOpen, onClose, project}: Props){
-  const [mount, setMount] = useState(false)
+export default function ModalProject({ isOpen, onClose, project }: Props) {
+  const [mount, setMount] = useState(false);
 
   useEffect(() => {
-    setMount(true)
-  },[])
+    setMount(true);
+  }, []);
   if (!mount) {
-    return <></>
+    return <></>;
   }
   return createPortal(
     <>
-    {isOpen && project &&
-      <>
-        <motion.div 
-        initial={{opacity: 0}}
-        animate={{opacity: 1}}
-        className=" flex fixed justify-center items-center z-50  h-screen  w-screen pointer-events-none overscroll-y-contain ">
-          <div className="relative overflow-y-auto pointer-events-auto bg-white rounded-lg w-full lg:max-w-2xl  h-auto max-h-screen  md:w-3/5 overflow-hidden bg-primaryLight dark:bg-secondaryDark ">
-            <div className="relative aspect-video">
-              <Image
-                className="object-cover"
-                alt="project image"
-                fill
-                src="https://criticalhits.com.br/wp-content/uploads/2020/09/anbu-minato-768x432.jpg"
-              />
-            </div>
-            <div className="flex flex-col gap-5 p-4">
-            <CloseIcon className="block md:hidden absolute top-0 right-0 " onClick={() => {onClose()}}/>
-
-              <h1 className="font-bold text-2xl md:text-title text-gray dark:text-whiteTextDarkMode  text-bold ">
-                {project.name}
-              </h1>
-              <span className="font-Inter font-medium mb-3 text-justify dark:text-grayLight">
-                {project.description}
-              </span>
-              <div className="flex flex-row  flex-wrap ">
-                <h3 className="pr-2 text-base">Stack:</h3>
-              {project.technologies.map((technology, i) => {
-                return (
-                  <>
-                    <h3 className="font-thin text-base">{technology}
-                    {
-                      i !== project.technologies.length - 1 
-                      && <span className="px-2 text-redLight">•</span>
-                    }
-                    </h3>
-                  </>
-                )
-              })}
+      {isOpen && project && (
+        <>
+          <motion.div
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            className="flex fixed justify-center items-center z-50  h-screen  w-screen pointer-events-none"
+          >
+            <div className="relative flex flex-col pointer-events-auto bg-white rounded-lg w-full lg:max-w-xl lg:max-h-[90vh]  h-auto max-h-screen  md:w-3/5 overflow-y-hidden bg-primaryLight dark:bg-secondaryDark ">
+              <div className="relative aspect-video shrink-0">
+                <Image
+                  className="object-cover "
+                  alt="project image"
+                  fill
+                  src="https://criticalhits.com.br/wp-content/uploads/2020/09/anbu-minato-768x432.jpg"
+                />
+                <CloseIcon
+                  className="block md:hidden absolute top-0 right-0 "
+                  onClick={() => {
+                    onClose();
+                  }}
+                />
               </div>
-              <a href={project.link} >
-                <h3 className=" w-fit px-2 py-1 font-thin rounded-sm text-redLight dark:text-redLight ">View Project</h3>
+              <div className="flex flex-col gap-5 p-4 overflow-y-auto grow pb-20">
+                <h1 className="font-bold text-2xl md:text-title text-gray dark:text-whiteTextDarkMode  text-bold ">
+                  {project.name}
+                </h1>
+                <span className="font-Inter font-medium mb-3 text-justify dark:text-grayLight">
+                  {project.description}
+                </span>
+                <div className="flex flex-row  flex-wrap ">
+                  <h3 className="pr-2 text-base">Stack:</h3>
+                  {project.technologies.map((technology, i) => {
+                    return (
+                      <>
+                        <h3 className="font-thin text-base">
+                          {technology}
+                          {i !== project.technologies.length - 1 && (
+                            <span className="px-2 text-redLight">•</span>
+                          )}
+                        </h3>
+                      </>
+                    );
+                  })}
+                </div>
+              </div>
+              <a
+                href={project.link}
+                className="absolute w-full bottom-0 bg-gradient-to-t from-primary via-primary to-[rgba(0,0,0,0)] pt-8 dark:from-secondaryDark dark:via-secondaryDark "
+              >
+                <h3 className=" w-fit px-2 py-1 font-thin rounded-sm text-redLight dark:text-redLight ">
+                  View Project
+                </h3>
               </a>
             </div>
-          </div>
           </motion.div>
-        <Backdrop onClose={() => {onClose()}} /></>}
-      </>, document.getElementById("modal-root")
-
+          <Backdrop
+            onClose={() => {
+              onClose();
+            }}
+          />
+        </>
+      )}
+    </>,
+    document.getElementById("modal-root"),
   );
 }
