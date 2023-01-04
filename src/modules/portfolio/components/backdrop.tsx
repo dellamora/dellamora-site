253a3ab7ee593a78363d9cff02a75f2dc9bf09a8
Dellamora <francielle@dellamora.dev>
2023-01-04 16:47:56 -0300

    eslint error fixed

--- src/modules/portfolio/components/backdrop.tsx
+++ src/modules/portfolio/components/backdrop.tsx
@@ -3,7 +3,6 @@
 import { motion } from "framer-motion";
 import React from "react";
 
-
 const Backdrop: React.FC<{ onClose: () => void }> = ({
   onClose,
 }): JSX.Element => {
@@ -12,7 +11,9 @@ const Backdrop: React.FC<{ onClose: () => void }> = ({
       initial={{ opacity: 0 }}
       animate={{ opacity: 1 }}
       exit={{ opacity: 0 }}
-      onClick={() => {onClose()}}
+      onClick={() => {
+        onClose();
+      }}
       className="z-40 fixed w-screen h-screen  bg-secondaryDark/70 "
     />
   );
