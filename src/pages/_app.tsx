4bfeb329194c21cbdb693a228dc906a2bd3aa536
Dellamora <francielle@dellamora.dev>
2023-01-05 11:29:14 -0300

    fixed darkmode context

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,16 @@
 import type { AppProps } from "next/app";
 import "../common/styles/globals.css";
 import BaseLayout from "../common/layouts/basedLayout";
-import { DarkModeContextProvider } from "../common/context/darkMode";
+import {
+  DarkModeContext,
+  DarkModeContextProvider,
+} from "../common/context/darkMode";
+import { useContext } from "react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 export default function App({ Component, pageProps }: AppProps): JSX.Element {
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const { isDark } = useContext(DarkModeContext);
   const queryClient = new QueryClient();
 
   return (
