f9697d4f15966da5f0882f67d5f41cd089acfdd2
Dellamora <franciellle@dellamora.dev>
2023-10-29 15:04:27 -0300

    change profile pic

--- src/modules/hero/cards/Cards.tsx
+++ src/modules/hero/cards/Cards.tsx
@@ -9,14 +9,14 @@ const Cards: React.FC = (): JSX.Element => {
         transition={{ duration: 2, delay: 0.75 }}
         className="
         bg-redLight
-          lg:w-[19rem] 
+          lg:w-[20rem] 
           lg:rounded-[16px] 
-          lg:h-[28rem] 
+          lg:h-[20rem] 
           "
       />
       <img
         src="/mePc.jpg"
-        className="hidden lg:block lg:absolute rounded-[16px] w-[19rem] h-[28rem]"
+        className="hidden lg:block lg:absolute rounded-[16px] w-[20rem] h-[20rem]"
       />
     </div>
   );
